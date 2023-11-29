03ad1b624d9f432e7ef34d5a7c8081629ef56efd
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-11-29 22:17:40 +0200

    update

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,7 @@
 for i in range(10):
     for x in range(i + 1, 10):
+     result = i * 10 + x
      print("{:02d}, ".format(i * 10 + x), end="") 
-print()
+     if result == 89:
+        print(result)
+print("\n")
