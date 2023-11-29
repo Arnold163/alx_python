88e06df2a2a7b6405c33afde55532ea4c14b16a4
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-11-29 20:35:12 +0200

    update

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,4 @@
 for i in range(10):
     for x in range(i + 1, 10):
-        print("{:02d}, " .format(i * 10 + x), end=" ")
+        print("{:02d}, " .format(i * 10 + x), end="")
 print ("\n")        
