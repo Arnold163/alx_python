8392605280508e4355ea775d22908ab010965179
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-11-30 12:20:35 +0200

    update

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,12 @@
+def fibonacci_sequence(n):
+    if n<= 0:
+        return []
+    elif n==1:
+        return[0]
+    else:
+        sequence = [0, 1]
+        for _ in range(2, n):
+            next_number = sequence[-1] + sequence[-2]
+            sequence.append(next_number)
+        return sequence
+        
