a3f487e2b894b9ee842a7deb1d91b9f2d86ee7a6
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-12-07 19:21:02 +0200

    update

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,11 +2,11 @@ import sys
 
 num_args = len(sys.argv) - 1
 if num_args == 0:
-    print("0 arguments.")
+    print("0 argument.")
 elif num_args == 1:
-    print("1 arguments:")
+    print("1 argument:")
 else: 
-    print("{} arguments:" .format(num_args))
+    print("{} argument:" .format(num_args))
 
 for i in range(1, num_args + 1):
     print("{}: {}" .format(i, sys.argv[i]), end="")
