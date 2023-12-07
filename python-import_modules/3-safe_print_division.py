94a487d1ebf880f249b3211164d948423297e0ed
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-12-07 16:16:51 +0200

    update

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,26 +1,50 @@
 def safe_print_division(a, b):
     
     try:
-        result = a/b
+        result = a / b
     except ZeroDivisionError:
         result = None
     finally:
-        print("inside result: {}" .format(result))
+        return result
+test_cases = [
+        (10, -2),
+        (0, 2),
+        (10, 0),
+        (0, 0),
+        (12, 2),
+        (12, 0),
+        (12, -2),
+        (10, 2)
+    ]
+for a, b in test_cases:
+    result = safe_print_division(a, b)
 
-a = 12
-b = 2
+if result is not None:
+    print("correct output - case: a = {} / b = {} = {}" .format(a, b, result))
+    print("{:d} / {:d} = {}" .format(a, b, result))
+else:
+    print("division by zero - case: a = {} / b = {}" . format(a, b))
 
-result =safe_print_division(a, b)
-print("{:d} / {:d} = {}" .format(a, b, result))
 
-a = 12 
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}" .format(a, b, result))
 
-a = 10 
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}" .format(a, b, result))
+
+    
+       # print("inside result: {}" .format(result))
+
+#a = 12
+#b = 2
+
+#result =safe_print_division(a, b)
+#print("{:d} / {:d} = {}" .format(a, b, result))
+
+#a = 12 
+#b = 0
+#result = safe_print_division(a, b)
+#print("{:d} / {:d} = {}" .format(a, b, result))
+
+#a = 10 
+#b = 0
+#result = safe_print_division(a, b)
+#print("{:d} / {:d} = {}" .format(a, b, result))
 
 
