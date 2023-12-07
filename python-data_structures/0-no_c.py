18a4a4d46de432b1796d04fa7205448a6c8b522d
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-12-07 21:09:22 +0200

    update

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,10 @@
+def no_c(my_string):
+    filtered_chars = [char for char in my_string if char.lower() not in ('c' 'C')]
+    result = '' .join(filtered_chars)
+    return result
+
+print(no_c("Holberton School"))
+print(no_c("Chicago"))
+print(no_c("C is Fun"))
+
+
---
+++ python-functions/tuples.py
@@ -0,0 +1,35 @@
+
+#a = [1, 2, 3, 4]
+#b = a
+#print(b)
+
+#a = [1, 2, 3, 4]
+#result =a[1:3]
+#print(result)
+
+"""a = [1, 2, 3, 4]
+result = a[-1]
+print(result)"""
+
+"""a = [1, 2, 3, 4]
+result = a[-3]
+print(result)"""
+
+
+"""a = [1, 2, 3, 4]
+b = a
+a[2] = 10
+print(b)"""
+
+"""a = [1, 2, 3, 4]
+a[2] = 10
+print(a)"""
+
+"""a = [1, 2, 3, 4]
+b = a
+a[2] = 10
+print(a)"""
+
+a = [1, 2, 3, 4]
+a[0]
+print([0])
