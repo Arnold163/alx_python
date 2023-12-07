fb419fbc905b50b26f44a49701d00f3d9782cc4b
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-12-07 19:05:33 +0200

    update

---
+++ python-import_modules/4-raise_exception.py
@@ -0,0 +1,8 @@
+def raise_exception():
+    raise TypeError("Type Exception")
+
+if __name__ == "__main__":
+    try:
+        raise_exception()
+    except TypeError as te:
+        print("Exception raised")
