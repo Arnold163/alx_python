e0e65a80fe41214b497002c20d3ba1313dc955dd
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-12-09 16:42:21 +0200

    update

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -6,10 +6,10 @@ def print_matrix_integer(matrix=[[]]):
             if i <len(row) - 1:
                 print("{:d} " .format(num), end="")
             else:
-                print("{:d}" .format(num))
+                print("{:d}" .format(num), end="")
 
     if row !=matrix[-1]:
-        print(end="")           
+        print(end=" ")           
 
 if __name__ == "__main__":
     matrix = [
