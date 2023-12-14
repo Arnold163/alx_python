ec0f7f2cb92fa46fec4be6d11c07ab50bb553b18
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-12-14 21:20:31 +0200

    update

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,3 +1,11 @@
 """base geometry"""
 class BaseGeometry:
     pass
+
+"""import base"""
+BaseGeometry = __import__('3-base_geometry').BaseGeometry
+bg = BaseGeometry()
+
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
