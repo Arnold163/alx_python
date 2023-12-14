67cc689189ce552eca6c9c8fd528afdadcd4ce26
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-12-14 21:10:19 +0200

    update

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -4,3 +4,7 @@ def inherits_from(obj, a_class):
     return issubclass(type(obj), a_class)
 
 inherits_from = __import__('2-inherits_from').inherits_from
+
+a = True
+if inherits_from(a, int):
+    print("{} inherited from class {}" .format(a, int.__name__))
