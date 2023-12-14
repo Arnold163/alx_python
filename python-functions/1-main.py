5c473e22703082e861f505052fc3972df1403d68
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-12-14 16:16:33 +0200

    update

--- python-functions/1-main.py
+++ python-functions/1-main.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
-pow = __import__('1-power').pow
+"""pow = __import__('1-power').pow
 
 print(pow(2, 2))
 print(pow(98, 2))
 print(pow(98, 0))
 print(pow(100, -2))
-print(pow(-4, 5))
+print(pow(-4, 5))"""
+
+
+
+class Base():
+    """ My base class """
+
+    __nb_instances = 0
+
+    def __init__(self):
+        Base.__nb_instances += 1
+        self.id = Base.__nb_instances
+
+class User(Base):
+    """ My User class """
+    pass
+
+b = Base()
+u = User()
+print(u.id)
+
