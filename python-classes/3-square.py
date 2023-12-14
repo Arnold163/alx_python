3e700dc6308888160806cbf67941480d9f14a18c
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-12-14 09:35:35 +0200

    update

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,6 +1,10 @@
 class Square:
     def __init__(self, size=0):
-        self.size = size
+        try:
+            self.size = size
+        except Exception as e:
+            print(e)
+
     @property
     def size(self):
         return self.__size
@@ -15,36 +19,32 @@ class Square:
             self.__size = value
 
     def area(self):
-        return self.__size ** 2
+        return self.__size ** 2   
     
-#my_square_1 = Square(3)
-#print("Area: {}" .format(my_square_1.area()))
-#print("size: {}".format(my_square_1.size))
-
 try:
-    my_square_1 = "invalid"
+    my_square_1 = Square("invalid")
 except Exception as e:
-        print(e)
+        pass
 
 try: 
     my_square_2 = Square(89)
     print(my_square_2.size)
     print(my_square_2.area())
 except Exception as e:
-    print(e)
+    pass
 
 try: 
     my_square_3 = Square(-33)
     print(my_square_3.size)
     print(my_square_3.area())
 except Exception as e:
-    print(e)
+    pass
 
 try: 
     my_square_4 = Square(33)
     print(my_square_4.size)
     print(my_square_4.area())
 except Exception as e:
-    print(e)
+    pass
 
      
--- python-data_structures/main_1.py
+++ python-data_structures/main_1.py
@@ -1,7 +1,62 @@
-no_c = __import__('0-no_c').no_c
+class Square:
+    def __init__(self, size=0):
+        try:
+            self.size = size
+        except Exception as e:
+            print(e)
 
-word = "School"
-new_word = no_c(word)
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
-print(new_word)
-print(word)
+    def area(self):
+        return self.__size ** 2   
+    
+try:
+    my_square_1 = Square("invalid")
+except Exception as e:
+    pass
+
+try: 
+    my_square_2 = Square(89)
+    print(my_square_2.size)
+    print(my_square_2.area())
+except Exception as e:
+    pass
+
+try: 
+    my_square_3 = Square(-33)
+    print(my_square_3.size)
+    print(my_square_3.area())
+except Exception as e:
+    pass
+
+try: 
+    my_square_4 = Square(33)
+    print(my_square_4.size)
+    print(my_square_4.area())
+except Exception as e:
+    pass
+
+
+
+
+
+
+
+#no_c = __import__('0-no_c').no_c
+
+#word = "School"
+#new_word = no_c(word)
+
+#print(new_word)
+#print(word)
