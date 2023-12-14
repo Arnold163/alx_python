0aac9e2ae04855072e4f2d9c3d5072781928cae1
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-12-14 21:38:45 +0200

    update

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -9,7 +9,7 @@ BaseGeometry = __import__('4-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
 
-"""try:
+try:
     print(bg.area())
 except Exception as e:
-    print("[{}] {}" .format(e.__class__.__name__, e))"""
+    print("[{}] {}" .format(e.__class__.__name__, e))
