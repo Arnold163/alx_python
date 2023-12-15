aaae5d3dc7757b7e6b8a697a153ae419c1bb89c5
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-12-15 18:55:21 +0200

    update

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -8,7 +8,7 @@ my_square = Square(3)
 print(type(my_square))
 print(my_square.__dict__)
 
-try:
+"""try:
     print(my_square.size)
 except Exception as e:
     print(e)
@@ -16,4 +16,4 @@ except Exception as e:
 try:
     print(my_square.__size)
 except Exception as e:
-    print(e)
+    print(e)"""
