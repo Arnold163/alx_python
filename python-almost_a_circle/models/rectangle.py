ab7a576eda560e5b5d6aa6ee55b88ac3801017cd
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-12-21 18:32:53 +0200

    update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -152,7 +152,6 @@ class Rectangle(Base):
             print()
         for _ in range(self.__height):
             print("" * self.__x + "#" * self.__width, end="")
-            print()
     
     def __str__(self):
         """
