55fb24152fedb8b2286aac4353f54e5478a8fd92
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2023-12-21 19:22:32 +0200

    update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -164,32 +164,7 @@ class Rectangle(Base):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
     
 
-    def update(self, *args):
-
-        """
-        Update the Rectangle attributes.
-
-        Args:
-            *args: Variable number of arguments in the order (id, width, height, x, y).
-        """
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
-
-#example of usage
-r = Rectangle(1, 2, 3, 4, 5)
-print(r) #before update
-
-r.update(10, 20, 30, 40, 50)
-print(r) #after update
-
+   
         
             
     
