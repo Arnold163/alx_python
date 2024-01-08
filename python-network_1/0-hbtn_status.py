0abb92629ccde14dd7cd5bcf20be900c0056a1ed
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-08 15:43:38 +0200

    update

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://alu-intranet.hbtn.io/status"
+
+try: 
+    response = requests.get(url)
+    response.raise_for_status() # Raise an exception for HTTP errors 
+
+    # displaying the response 
+    print("-" * 50)
+    print("Body response:")
+    print("\t- type:" , type(response.text))
+    print("\t- content:" , response.text)
+
+except requests.exceptions.RequestException as e:
+    print(f"Error fetching the URL: {e}")
