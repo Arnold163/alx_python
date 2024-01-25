7d7fb28a5ee6194a24376b4d6b80c4bbe4d5816a
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-25 14:00:47 +0200

    update

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -2,7 +2,7 @@
 import MySQLdb
 from sys import argv
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     if len(argv) != 5:
         print("usage: {}<username> <password> <database> <state_name>".format(argv[0]))
         exit(1)
