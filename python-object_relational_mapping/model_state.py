be9b10d575f69ce250a70b95e2e7fb9aa817eee6
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-25 23:31:31 +0200

    update

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 """first state model"""
 import sys
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
