d27ef720d96f924690ea4652f90f39f81f1e63f6
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-25 23:29:43 +0200

    first model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,21 +1,16 @@
-#first state model
-from sqlalchemy import Column, Integer, String, create_engine 
+"""first state model"""
+import sys
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 class State(Base):
+    """state class"""
     __tablename__ = 'states'
-
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
 
-    engine = create_engine('mysl://username:password@localhost:3306/database')
-
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    session.close()
