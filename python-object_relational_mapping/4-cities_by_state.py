549bf17b3ca05ea109160a1de59f1a7bada20309
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-25 11:38:56 +0200

    cities

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#sql injection lets inject this
+import MySQLdb
+from sys import argv
+
+if __name__ == "__name__":
+    if len(argv) != 5:
+        print("usage: {}<username> <password> <database> <state_name>".format(argv[0]))
+        exit(1)
+    username, password, database, state_name = argv[1], argv[2], argv[3], argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+
+    data = cursor.fetchall()
+    
+
+    for row in data:
+        print(row)
+
+    cursor.close()
+    db.close()
