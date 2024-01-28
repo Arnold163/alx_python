6619f923dd4aadaf64f102e6f994ba6de0001444
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-28 16:46:51 +0200

    HBNB

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#lets on flask now
+""" maybe this type of documantation?"""
+from flask import Flask
+
+#create a flask app instance
+app = Flask(__name__)
+""" maybe this type of documantation?"""
+#define and use strict
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    #what else am i to document here
+    return 'Hello HBNB!'
+#run the flask app if script is executed 
+""" maybe this type of documantation?"""
+def hbnb():
+    return 'HBNB'
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
