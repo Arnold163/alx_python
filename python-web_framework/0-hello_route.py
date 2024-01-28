a1326d5146560b18a85bfb06673af09f99150205
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-28 16:38:56 +0200

    update

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,14 +1,16 @@
 #lets on flask now
+""" maybe this type of documantation?"""
 from flask import Flask
 
 #create a flask app instance
 app = Flask(__name__)
-
+""" maybe this type of documantation?"""
 #define and use strict
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     #what else am i to document here
     return 'Hello HBNB!'
 #run the flask app if script is executed 
+""" maybe this type of documantation?"""
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
