193eeb8452175b12c41a903c6c8d399bfe21c6c5
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-28 16:19:34 +0200

    update

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,11 +1,13 @@
 #lets on flask now
 from flask import Flask
 
+#create a flask app instance
 app = Flask(__name__)
 
-@pp.route('/', strict_slashes=False)
+#define and use strict
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
-
+#run the flask app if script is executed 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
