b2d678778fdd49cbbc4b923d4da598da02fd816b
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-30 04:10:59 +0200

    update

--- python-web_framework/9-update_delete_users.py
+++ python-web_framework/9-update_delete_users.py
@@ -83,7 +83,7 @@ if len(sys.argv) != 4:
     
     if request.method == 'POST':
         try:
-            db.session.delete_user(user)
+            db.session.delete(user)
             db.session.commit()
             flash("User deleted successfully", "success")
         except Exception as e:
