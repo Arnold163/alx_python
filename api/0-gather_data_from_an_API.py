e749ea0f10bc7161fce8990136a5215b0078beda
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-02-29 23:17:58 +0200

    update

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,24 +1,29 @@
-# gathering data from an API
 import requests
 import sys
 
 def fetch_todo_list_progress(employee_id):
-    # employee details
-    employee_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    # Fetching employee details
+    employee_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}")
     employee_data = employee_response.json()
     employee_name = employee_data['name']
 
-    #todo list for the employee
-    todo_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+
+    # Fetching todo list for the employee
+    todo_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
     todo_data = todo_response.json()
 
-    #counting completed tasks
+
+    # Counting completed tasks
     completed_tasks = [task for task in todo_data if task['completed']]
     num_completed_tasks = len(completed_tasks)
     total_tasks = len(todo_data)
 
-    #Printing employee todo list progress
-    print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
+
+    # Printing employee todo list progress
+    print(
+        f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t{task['title']}")
 
