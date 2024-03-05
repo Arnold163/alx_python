02cb8494a00b3e0b3542e0f94bfd37214db9830c
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-03-05 16:20:04 +0200

    update

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -23,7 +23,7 @@ def fetch_todo_list_progress(employee_id):
 
     # Printing employee todo list progress
     print(
-        f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
+        f"Employee {employee_name}{' '*(9-len(employee_name))} is done with tasks ({num_completed_tasks}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t{task['title']}")
 
