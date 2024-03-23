6bccaadc3fb6ca19af4b79ea832b155515229c4b
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-03-23 16:21:17 +0200

    update

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,19 +1,11 @@
 """this stuff is not easy i have less time aargh"""
-Square = __import__('0-square').Square
+class Square:
+    def __init__(self, size):
+        self.__size = size
 
 my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
+print(type(my_square), end="")
+print()
 
 
 
