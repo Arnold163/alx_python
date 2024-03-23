195d25f5ceccf94879de658cabb71798dd471144
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-03-23 16:20:01 +0200

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,19 @@
 """this stuff is not easy i have less time aargh"""
-class Square:
-    def __init__(self, size):
-        self.__size = size
+Square = __import__('0-square').Square
 
 my_square = Square(3)
-print(type(my_square), end="")
-print()
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
 
 
 
