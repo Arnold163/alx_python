922b5cec50bfebaccc9e4a376a0edd5f6fe26ae1
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-03-23 16:36:03 +0200

    update

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,8 +3,12 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-my_square = Square(3)
-print(f"<class '{type(my_square).__module__}.{type(my_square).__name__}'>\n{vars(my_square)}")
+mysquare = Square(3) 
+print(type(mysquare)) 
+print(mysquare.dict_)
+
+#my_square = Square(3)
+#print(f"<class '{type(my_square).__module__}.{type(my_square).__name__}'>\n{vars(my_square)}")
 #print(vars(my_square))
 
 
