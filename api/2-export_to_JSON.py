0f08efddcd0084acb6fb4673a3a5454d4e772c12
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-03-25 10:54:25 +0200

    update

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -38,6 +38,8 @@ def fetch_todo_list_progress(employee_id):
     for task in completed_tasks:
         print(f"\t{task['title']}")
 
+    print(f"Correct {employee_id}: OK")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
