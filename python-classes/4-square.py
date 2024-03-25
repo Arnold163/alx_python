5d7cbbeb142c9751228fd3a9343015dad884b432
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-03-25 16:52:36 +0200

    update

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,29 +1,29 @@
 """ is this documanted enough man?"""
-# class square
 class Square:
-    
-## A class representing a square.
+    """
+    A class representing a square.
 
-#Attributes:
-#__size (int): The size of the square.
-
+    Attributes:
+        __size (int): The size of the square.
+    """
 
     def __init__(self, size=0):
-       # 
-       # Initializes a Square instance with a specified size.
+        """
+        Initializes a Square instance with a specified size.
 
-       # Args:
-       #    size (int): The size of the square (default is 0).
+        Args:
+            size (int): The size of the square (default is 0).
+        """
         self.size = size
 
     @property
     def size(self):
-        
-        #Getter method for the size attribute.
+        """
+        Getter method for the size attribute.
 
-        #Returns:
-        #    int: The size of the square.
-        
+        Returns:
+            int: The size of the square.
+        """
         return self.__size
 
     @size.setter
@@ -46,24 +46,23 @@ class Square:
             self.__size = value
 
     def area(self):
-        
-       # Calculates the area of the square.
+        """
+        Calculates the area of the square.
 
-        #Returns:
-        #    int: The area of the square.
-    
-       # return self.__size ** 2
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
 
-     def my_print(self):
-        
-        #Prints the square pattern using '#'.
-        
+    def my_print(self):
+        """
+        Prints the square pattern using '#'.
+        """
         if self.__size == 0:
             print()
         else:
             for _ in range(self.__size):
                 print("#" * self.__size)
-
 
 
         
